7c5732039cf60d8dad5f71a52633a965f433b833
xdd <45672942@qq.com>
2023-12-09 22:28:55 +0800

    feat(dashboard): add dashboard component

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className={'bg-gray-200'}>
+            <h1 className={'text-3xl text-blue-600'}>
+                Hello Dashboard Layout 👋
+            </h1>
+            {children}
+        </div>
+    )
+}
