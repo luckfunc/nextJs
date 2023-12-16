ace438788562adbb642f49fc59c726ac32f5eb87
xdd <45672942@qq.com>
2023-12-17 00:17:31 +0800

    feat: format code

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,14 @@
 export default function DashboardLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    return (
-        <div className={'bg-gray-200'}>
-            <h1 className={'text-3xl text-blue-600'}>
-                Hello Dashboard Layout 👋
-            </h1>
-            {children}
-        </div>
-    )
+  return (
+    <div className={'bg-gray-200'}>
+      <h1 className={'text-3xl text-blue-600'}>
+        Hello Dashboard Layout 👋
+      </h1>
+      {children}
+    </div>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,29 @@ import Link from 'next/link';
 import PostListLink from '@/app/blogs/PostList';
 
 export default function Home() {
-    const posts = [
-        {
-            id: 1,
-            title: 'Image'
-        },
-        {
-            id: 2,
-            title: 'Post 2'
-        },
-        {
-            id: 3,
-            title: 'Post 3'
-        }
-    ];
-  return (
-      <>
-          <h1 className={'text-4xl text-orange-600'}>
-              Hello Home Page
-          </h1>
-          <Link href='/dashboard'>
-              Go to Dashboard
-          </Link>
-          <PostListLink posts={posts} />
-      </>
-  )
+	const posts = [
+		{
+			id: 1,
+			title: 'Image'
+		},
+		{
+			id: 2,
+			title: 'Post 2'
+		},
+		{
+			id: 3,
+			title: 'Post 3'
+		}
+	];
+	return (
+		<>
+			<h1 className={'text-4xl text-orange-600'}>
+				Hello Home Page
+			</h1>
+			<Link href='/dashboard'>
+				Go to Dashboard
+			</Link>
+			<PostListLink posts={posts} />
+		</>
+	)
 }
